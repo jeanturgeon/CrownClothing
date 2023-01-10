c780f45674f8de50129de83e84150fc92656050f
Jean Turgeon <fotojt@gmail.com>
2023-01-10 16:47:11 -0500

    Update firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAea2VsHMiTgK8qlnaFTKEZHcuOVplCp98",
-  authDomain: "crown-clothing-db-799a2.firebaseapp.com",
-  projectId: "crown-clothing-db-799a2",
-  storageBucket: "crown-clothing-db-799a2.appspot.com",
-  messagingSenderId: "327846440016",
-  appId: "1:327846440016:web:a6904d5fb78a2b0266b555",
-};
+import { firebaseConfig } from "../../../firebaseConfig";
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
