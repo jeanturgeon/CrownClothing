59bfa7222c865c8a3f76897f23649ed31fe76098
Jean Turgeon <fotojt@gmail.com>
2023-01-11 13:01:02 -0500

    Added Firebase Auth by gmail and setup Firestore to save user in firebaseDB

--- src/routes/auth/sign-in.component.js
+++ src/routes/auth/sign-in.component.js
@@ -1,10 +1,12 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup, createUserDocFromAuth } from "../../utils/firebase/firebase.utils";
+
+
 
 
 export default function SignIn() {
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
-        console.log(response);
+        const userDocRef = await createUserDocFromAuth(response.user);
     }
 
     return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 
-import { firebaseConfig } from "../../../firebaseConfig";
+import { firebaseConfig } from "../../firebaseConfig";
 
 // Initialize FirebaseApp
 const firebaseApp = initializeApp(firebaseConfig);
@@ -13,3 +14,32 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const firebaseDB = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) => {
+     //create document reference:
+    const userDocRef = doc(firebaseDB, 'users', userAuth.uid);
+
+     //see if that reference already exists, then creating it if it doesn't
+    const userSnapshot = await getDoc(userDocRef);
+    if(!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log("Error creating user: ", error.message);
+        }
+    }
+
+    //if it does exist, then we just return it:
+    return userDocRef;
+
+
+};
