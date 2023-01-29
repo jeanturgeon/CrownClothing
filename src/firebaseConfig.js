1efab739db6725cc8fc8b3c53b69b349a0c8d1cb
Jean Turgeon <fotojt@gmail.com>
2023-01-29 15:11:38 -0500

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAea2VsHMiTgK8qlnaFTKEZHcuOVplCp98",
-    authDomain: "crown-clothing-db-799a2.firebaseapp.com",
-    projectId: "crown-clothing-db-799a2",
-    storageBucket: "crown-clothing-db-799a2.appspot.com",
-    messagingSenderId: "327846440016",
-    appId: "1:327846440016:web:a6904d5fb78a2b0266b555"
-  };
-  
